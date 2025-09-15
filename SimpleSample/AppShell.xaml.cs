dc76c66c1ce6fffbf6f0b9c2cec56bc0890a17c9
RiyashameedM <riyashameed.m@syncfusion.com>
2025-09-15 14:36:14 +0530

    Uploaded

---
+++ SimpleSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SimpleSample/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace SimpleSample;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void tabScroll_Scrolled(object sender, ScrolledEventArgs e)
+	{
+		if (hstack.Children.Count < 10)
+		{
+			hstack.Children.Add(new Grid
+			{
+				HeightRequest = 48,
+				WidthRequest = 200,
+				BackgroundColor = Colors.Red,
+				Margin = new Thickness(2, 0),
+				Children =
+				{
+					new Label
+					{
+						Text = "text 9",
+						HorizontalOptions = LayoutOptions.Center,
+						VerticalOptions = LayoutOptions.Center,
+						TextColor = Colors.White
+					}
+				}
+			});
+		}
+    }
+}
+
